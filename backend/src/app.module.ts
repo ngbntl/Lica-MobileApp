5981c9a18a6087509ed95dfd51614bb039d18272
ngbntl <giabao5443@gmail.com>
2024-05-19 10:32:06 +0700

    entity users,cards,topics

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+import { CardsModule } from './modules/cards/cards.module';
+import { TopicsModule } from './modules/topics/topics.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URI'),
+        dbName: configService.get<string>('DATABASE_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    CardsModule,
+    TopicsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
