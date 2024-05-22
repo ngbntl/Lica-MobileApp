b0491a22d942b7a3a8397e055624068fd96a70ec
ngbntl <giabao5443@gmail.com>
2024-05-23 02:16:25 +0700

    database connected 🫶

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,29 +1,42 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { database_config } from './configs/configuration.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 import * as Joi from 'joi';
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'prodution', 'test', 'provision', 'staging')
-          .default('development'),
-        PORT: Joi.number().default(3000),
-      }),
-      validationOptions: {
-        abortEarly: false,
-      },
-      isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
-      load: [database_config],
-      cache: true,
-      expandVariables: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			validationSchema: Joi.object({
+				NODE_ENV: Joi.string()
+					.valid('development', 'prodution', 'test', 'provision', 'staging')
+					.default('development'),
+				PORT: Joi.number().default(3000),
+			}),
+			validationOptions: {
+				abortEarly: false,
+			},
+			isGlobal: true,
+			envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
+			load: [database_config],
+			cache: true,
+			expandVariables: true,
+		}),
+
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				uri: configService.get<string>('DATABASE_URI'),
+				dbName: configService.get<string>('DATABASE_NAME'),
+			}),
+			inject: [ConfigService],
+		}),
+
+		UsersModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
